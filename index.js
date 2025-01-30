b76114f10d654effb3105c992c91330402156b35
Thomas Robinson <git@tomr.me>
2025-01-30 20:12:04 +0000

    match jack's progress: done tests 1-9

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+
 /*
  * This is index.js
  * Open index.html in your browser to check what
@@ -10,4 +12,44 @@
 
 'use strict';
 
-// add your functions here
+function replaceText(elem, str) {
+  elem.innerHTML = str;
+}
+
+function addTextTo(elem, str) {
+  elem.innerHTML = `${elem.innerHTML}${str}`;
+}
+
+function moreBears() {
+  document.getElementById('animals').src = 'http://placebear.com/400/200';
+  document.getElementById('animals').alt = 'A bear.';
+  document.getElementById('animals').title = 'A BEAR!';
+}
+
+function setId(elem, str) {
+  elem.id = str;
+  return elem;
+}
+
+function setClass(elem, str) {
+  elem.classList = str;
+  return elem;
+}
+
+function addAClass(elem, str) {
+  elem.classList.add(str);
+  return elem;
+}
+
+function removeAClass(elem, str) {
+  elem.classList.remove(str);
+  return elem;
+}
+
+function newElement(name) {
+  return document.createElement(name);
+}
+
+function findElementById(id) {
+  return document.getElementById(id);
+}
